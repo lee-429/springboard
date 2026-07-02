392d45267ca454e279d78383fc354a001cc37e55
leehyunhak <exprim114@gmail.com>
2026-07-02 22:41:16 +0900

    feat: BoardRepository 인터페이스 생성

--- src/main/java/com/hyunhak/springboard/repository/BoardRepository.java
+++ src/main/java/com/hyunhak/springboard/repository/BoardRepository.java
@@ -5,13 +5,18 @@ import java.util.ArrayList;
 
 public interface BoardRepository {
 
+    // 게시글 저장
     Board save(Board board);
 
+    // 게시글 전체 조회
     ArrayList<Board> findAll();
 
+    // 게시글 ID 별 조회
     Board findById(Long id);
 
+    // 게시글 수정
     Board update(Board board);
 
+    // 게시글 삭제
     void delete(Long id);
 }
